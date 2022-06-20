94c76699e27f7fc7f76b3fa398206d2e2eb1e1a6
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-06-20 19:37:24 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// for token 32 caracters https://generate-secret.vercel.app/32
+// for token 64 caracters https://generate-secret.vercel.app/64
+// please change this token by an example above
+export const token =  "7f59ba5f69d5b4e588e0ab0d4f8e1634";
